2e25c4eeb80461a55487e303579c430215720a77
Manoj <manoj@mm.st>
2012-05-20 17:18:22 +0530

    .

--- sysinfo/src/manojvictormathew/sysinfo/SysinfoActivity.java
+++ sysinfo/src/manojvictormathew/sysinfo/SysinfoActivity.java
@@ -9,6 +9,5 @@ public class SysinfoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //comment2
     }
 }
