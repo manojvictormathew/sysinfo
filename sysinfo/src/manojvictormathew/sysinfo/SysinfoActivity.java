ebdd4284a771eec2d917e690a4a1729f172e9bd9
Manoj <manoj@mm.st>
2012-05-27 21:11:41 +0530

    Catch a string from main screen

--- sysinfo/src/manojvictormathew/sysinfo/SysinfoActivity.java
+++ sysinfo/src/manojvictormathew/sysinfo/SysinfoActivity.java
@@ -2,13 +2,36 @@ package manojvictormathew.sysinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SysinfoActivity extends Activity {
-    /** Called when the activity is first created. */
+    private OnClickListener buttoListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			EditText editText = (EditText) findViewById(R.id.editText1);
+			Log.d( "Sysinfo", editText.getText().toString());
+			
+		}
+	}; 
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //comment3
+        
+        TextView mainView = (TextView) findViewById(R.id.SysinfoActivityTextView);
+        mainView.setText(R.string.test_updating_a_view);
+        
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(buttoListener);
+        
+        
     }
 }
